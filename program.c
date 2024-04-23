37a7d4bc2c4073dd6945fa3205feb03bc40bd249
ArijitGit <sarkararijit110@gmail.com>
2024-04-23 17:32:26 +0530

    first code entered

---
+++ program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+    printf("Enter the number of times you want to print : ");
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++){
+        printf("Hello_Git");
+    }
+    
+    return 0;
+}
